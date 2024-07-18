3d309d24c316682c03ad163e916c4c610e6ba6a9
Florent Maitre <florent.maitre@orange.com>
2024-07-18 16:06:26 +0200

    Review: Remove duplicated file header

--- buildSrc/src/main/kotlin/com/orange/ouds/gradle/library.gradle.kts
+++ buildSrc/src/main/kotlin/com/orange/ouds/gradle/library.gradle.kts
@@ -11,17 +11,6 @@
  */
 
 package com.orange.ouds.gradle
-/*
- * Software Name: Orange Unified Design System
- * SPDX-FileCopyrightText: Copyright (c) Orange SA
- * SPDX-License-Identifier: MIT
- *
- * This software is distributed under the MIT license,
- * the text of which is available at https://opensource.org/license/MIT/
- * or see the "LICENSE" file for more details.
- *
- * Software description: Android library of reusable graphical components 
- */
 
 import gradle.kotlin.dsl.accessors._cb41af676f88d94563fb18deaa5153de.android
 import gradle.kotlin.dsl.accessors._cb41af676f88d94563fb18deaa5153de.kotlin
