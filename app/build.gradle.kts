379d652cd9e24ebb8370cda01491f37aefa7b64d
Florent Maitre <florent.maitre@orange.com>
2024-07-18 19:01:48 +0200

    Review: Add copyright

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,15 @@
+/*
+ * Software Name: Orange Unified Design System
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license,
+ * the text of which is available at https://opensource.org/license/MIT/
+ * or see the "LICENSE" file for more details.
+ *
+ * Software description: Android library of reusable graphical components
+ */
+
 plugins {
     id(libs.plugins.android.application.get().pluginId) // https://github.com/gradle/gradle/issues/20084#issuecomment-1060822638
     alias(libs.plugins.compose.compiler)
