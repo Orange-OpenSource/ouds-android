1972143f17beeae0e3fed2888fbe8a7924e23ef5
Florent Maitre <florent.maitre@orange.com>
2024-07-30 11:05:33 +0200

    Distribute development version of demo app with Firebase App Distribution

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,4 +13,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.firebase.appdistribution) apply false
+    alias(libs.plugins.google.services) apply false
 }
