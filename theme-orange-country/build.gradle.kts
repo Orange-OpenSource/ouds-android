68590380dfea0fd3fde2c6bae061c897c5a90fd2
Pauline Auvray <pauline.auvray@orange.com>
2024-08-01 11:54:27 +0200

    Rename theme-orange-derived in theme-orange-country

--- theme-orange-country/build.gradle.kts
+++ theme-orange-country/build.gradle.kts
@@ -27,4 +27,7 @@ android {
 dependencies {
     implementation(project(":theme-contract"))
     implementation(project(":theme-orange"))
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.material3)
 }
