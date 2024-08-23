f3c260fddbf56e4cece70a6428aa0ba3c1c5e8d3
Pauline Auvray <pauline.auvray@orange.com>
2024-08-23 16:11:07 +0200

    Set up android tests

---
+++ core/src/androidTest/java/com/orange/ouds/core/component/button/OudsButtonTest.kt
@@ -0,0 +1,43 @@
+/*
+ * Software Name: OUDS Android
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license,
+ * the text of which is available at https://opensource.org/license/MIT/
+ * or see the "LICENSE" file for more details.
+ *
+ * Software description: Android library of reusable graphical components
+ */
+
+package com.orange.ouds.core.component.button
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.orange.ouds.core.component.extension.setOdsContent
+import org.junit.Rule
+import org.junit.Test
+
+internal class OudsButtonTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun oudsButtonRendersAsExpected() {
+        with(composeTestRule) {
+            val text = "Click"
+            val onClick = {}
+
+            setOdsContent {
+                OudsButton(
+                    text = text,
+                    onClick = onClick,
+                )
+            }
+
+            onNodeWithText(text).assertIsDisplayed()
+        }
+    }
+}
