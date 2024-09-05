5ee7fbfe9923f7e312134b9de6411f4ebdb558fd
Pauline Auvray <pauline.auvray@orange.com>
2024-09-05 16:53:35 +0200

    Review: Uppercase all hex letters in color raw tokens values

--- theme-white-label/src/main/java/com/orange/ouds/theme/whitelabel/tokens/WhiteLabelColorRawTokens.kt
+++ theme-white-label/src/main/java/com/orange/ouds/theme/whitelabel/tokens/WhiteLabelColorRawTokens.kt
@@ -1,13 +1,13 @@
 /*
- * Software Name: OUDS Android
+ * SoFtware Name: OUDS Android
  * SPDX-FileCopyrightText: Copyright (c) Orange SA
- * SPDX-License-Identifier: MIT
+ * SPDX-License-IdentiFier: MIT
  *
- * This software is distributed under the MIT license,
- * the text of which is available at https://opensource.org/license/MIT/
- * or see the "LICENSE" file for more details.
+ * This soFtware is distributed under the MIT license,
+ * the text oF which is available at https://opensource.org/license/MIT/
+ * or see the "LICENSE" File For more details.
  *
- * Software description: Android library of reusable graphical components
+ * SoFtware description: Android library oF reusable graphical components
  */
 
 package com.orange.ouds.theme.whitelabel.tokens
@@ -15,51 +15,51 @@ package com.orange.ouds.theme.whitelabel.tokens
 import androidx.compose.ui.graphics.Color
 
 object WhiteLabelColorRawTokens {
-    val primary20 = Color(0xff00315f)
-    val primary30 = Color(0xff006cc5)
-    val primary40 = Color(0xff005095)
-    val primary80 = Color(0xffa5c8ff)
-    val primary90 = Color(0xff1675d1)
+    val primary20 = Color(0xFF00315F)
+    val primary30 = Color(0xFF006CC5)
+    val primary40 = Color(0xFF005095)
+    val primary80 = Color(0xFFA5C8FF)
+    val primary90 = Color(0xFF1675D1)
 
-    val secondary10 = Color(0xff2a373c)
-    val secondary20 = Color(0xff253238)
-    val secondary30 = Color(0xffaab8bf)
-    val secondary40 = Color(0xff536067)
-    val secondary80 = Color(0xffd4e2e9)
-    val secondary90 = Color(0xffb9c7ce)
+    val secondary10 = Color(0xFF2A373C)
+    val secondary20 = Color(0xFF253238)
+    val secondary30 = Color(0xFFAAB8BF)
+    val secondary40 = Color(0xFF536067)
+    val secondary80 = Color(0xFFD4E2E9)
+    val secondary90 = Color(0xFFB9C7CE)
 
-    val tertiary20 = Color(0xff003061)
-    val tertiary30 = Color(0xff599bf6)
-    val tertiary40 = Color(0xff005eb3)
-    val tertiary80 = Color(0xffa7c8ff)
-    val tertiary90 = Color(0xff79aeff)
-    val tertiary10 = Color(0xff002247)
+    val tertiary20 = Color(0xFF003061)
+    val tertiary30 = Color(0xFF599BF6)
+    val tertiary40 = Color(0xFF005EB3)
+    val tertiary80 = Color(0xFFA7C8FF)
+    val tertiary90 = Color(0xFF79AEFF)
+    val tertiary10 = Color(0xFF002247)
 
-    val error20 = Color(0xff68000f)
-    val error30 = Color(0xff9f001c)
-    val error40 = Color(0xff880016)
-    val error80 = Color(0xffffb3af)
-    val error90 = Color(0xffc61b2c)
+    val error20 = Color(0xFF68000F)
+    val error30 = Color(0xFF9F001C)
+    val error40 = Color(0xFF880016)
+    val error80 = Color(0xFFFFB3AF)
+    val error90 = Color(0xFFC61B2C)
 
-    val neutral6 = Color(0xff131314)
-    val neutral20 = Color(0xff303031)
-    val neutral24 = Color(0xff393939)
-    val neutral90 = Color(0xffe4e2e2)
-    val neutral94 = Color(0xfff0eded)
-    val neutral95 = Color(0xfff2f0f0)
-    val neutral98 = Color(0xfffbf9f9)
+    val neutral6 = Color(0xFF131314)
+    val neutral20 = Color(0xFF303031)
+    val neutral24 = Color(0xFF393939)
+    val neutral90 = Color(0xFFE4E2E2)
+    val neutral94 = Color(0xFFF0EDED)
+    val neutral95 = Color(0xFFF2F0F0)
+    val neutral98 = Color(0xFFFBF9F9)
 
-    val neutralVariant30 = Color(0xff444748)
-    val neutralVariant50 = Color(0xff747878)
-    val neutralVariant60 = Color(0xff8e9192)
-    val neutralVariant80 = Color(0xffc4c7c7)
-    val neutralVariant90 = Color(0xffc4c7c7)
+    val neutralVariant30 = Color(0xFF444748)
+    val neutralVariant50 = Color(0xFF747878)
+    val neutralVariant60 = Color(0xFF8E9192)
+    val neutralVariant80 = Color(0xFFC4C7C7)
+    val neutralVariant90 = Color(0xFFC4C7C7)
 
-    val white = Color(0xffffffff)
-    val black = Color(0xff000000)
+    val white = Color(0xFFFFFFFF)
+    val black = Color(0xFF000000)
 
-    val info = Color(0xff4170D8)
-    val yellow = Color(0xffffcc00)
-    val green = Color(0xff43A047)
-    val red = Color(0xffB00020)
+    val info = Color(0xFF4170D8)
+    val yellow = Color(0xFFFFCC00)
+    val green = Color(0xFF43A047)
+    val red = Color(0xFFB00020)
 }
