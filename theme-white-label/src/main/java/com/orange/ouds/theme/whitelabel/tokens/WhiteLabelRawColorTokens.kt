41f4ea6d17cec89f1577c3d0e0846e22135ced27
Pauline Auvray <pauline.auvray@orange.com>
2024-09-06 16:26:12 +0200

    Review: Fix headers

--- theme-white-label/src/main/java/com/orange/ouds/theme/whitelabel/tokens/WhiteLabelRawColorTokens.kt
+++ theme-white-label/src/main/java/com/orange/ouds/theme/whitelabel/tokens/WhiteLabelRawColorTokens.kt
@@ -1,13 +1,13 @@
 /*
- * SoFtware Name: OUDS Android
+ * Software Name: OUDS Android
  * SPDX-FileCopyrightText: Copyright (c) Orange SA
- * SPDX-License-IdentiFier: MIT
+ * SPDX-License-Identifier: MIT
  *
- * This soFtware is distributed under the MIT license,
- * the text oF which is available at https://opensource.org/license/MIT/
- * or see the "LICENSE" File For more details.
+ * This software is distributed under the MIT license,
+ * the text of which is available at https://opensource.org/license/MIT/
+ * or see the "LICENSE" file for more details.
  *
- * SoFtware description: Android library oF reusable graphical components
+ * Software description: Android library of reusable graphical components
  */
 
 package com.orange.ouds.theme.whitelabel.tokens
