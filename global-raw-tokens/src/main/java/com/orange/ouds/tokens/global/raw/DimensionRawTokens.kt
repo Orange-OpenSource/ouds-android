9940f57c8d6fadebe273c82327533a5faf89816b
Pauline Auvray <pauline.auvray@orange.com>
2024-09-18 11:34:07 +0200

    Add dimension raw tokens

--- global-raw-tokens/src/main/java/com/orange/ouds/tokens/global/raw/DimensionRawTokens.kt
+++ global-raw-tokens/src/main/java/com/orange/ouds/tokens/global/raw/DimensionRawTokens.kt
@@ -14,4 +14,38 @@ package com.orange.ouds.tokens.global.raw
 
 object DimensionRawTokens {
     const val dimensionBase = 4
+
+    const val dimension0 = dimensionBase * 0
+    const val dimension25 = dimensionBase * 0.5
+    const val dimension50 = dimensionBase * 1
+    const val dimension75 = dimensionBase * 1.5
+    const val dimension100 = dimensionBase * 2
+    const val dimension150 = dimensionBase * 3
+    const val dimension200 = dimensionBase * 4
+    const val dimension250 = dimensionBase * 5
+    const val dimension300 = dimensionBase * 6
+    const val dimension350 = dimensionBase * 7
+    const val dimension400 = dimensionBase * 8
+    const val dimension450 = dimensionBase * 9
+    const val dimension500 = dimensionBase * 10
+    const val dimension550 = dimensionBase * 11
+    const val dimension600 = dimensionBase * 12
+    const val dimension650 = dimensionBase * 13
+    const val dimension700 = dimensionBase * 14
+    const val dimension750 = dimensionBase * 15
+    const val dimension800 = dimensionBase * 16
+    const val dimension900 = dimensionBase * 18
+    const val dimension1000 = dimensionBase * 20
+    const val dimension1200 = dimensionBase * 24
+    const val dimension1400 = dimensionBase * 28
+    const val dimension1600 = dimensionBase * 32
+    const val dimension1800 = dimensionBase * 36
+    const val dimension2000 = dimensionBase * 40
+    const val dimension3000 = dimensionBase * 80
+    const val dimension4000 = dimensionBase * 120
+    const val dimension5000 = dimensionBase * 140
+    const val dimension6000 = dimensionBase * 160
+    const val dimension7000 = dimensionBase * 180
+    const val dimension9000 = dimensionBase * 220
+    const val dimension11000 = dimensionBase * 260
 }
