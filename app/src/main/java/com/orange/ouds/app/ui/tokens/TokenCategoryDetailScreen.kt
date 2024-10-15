63de880472876063092a91a46ef50f015c597a2e
Pauline Auvray <pauline.auvray@orange.com>
2024-10-15 12:00:56 +0200

    Review: Remove useless line

--- app/src/main/java/com/orange/ouds/app/ui/tokens/TokenCategoryDetailScreen.kt
+++ app/src/main/java/com/orange/ouds/app/ui/tokens/TokenCategoryDetailScreen.kt
@@ -23,7 +23,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
@@ -51,8 +50,7 @@ fun TokenCategoryDetailScreen(tokenCategory: TokenCategory) {
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = OudsSpacingFixedKeyToken.Medium.value, vertical = OudsSpacingFixedKeyToken.Shorter.value),
-                    verticalAlignment = Alignment.Top
+                        .padding(horizontal = OudsSpacingFixedKeyToken.Medium.value, vertical = OudsSpacingFixedKeyToken.Shorter.value)
                 ) {
                     when (tokenCategory) {
                         is TokenCategory.Opacity -> tokenCategory.Illustration(opacity = token.value as Float)
