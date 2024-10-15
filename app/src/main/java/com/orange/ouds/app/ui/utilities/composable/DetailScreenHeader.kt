5b9fbba72641e0d4f0839944467f8eefa9be2fb7
Pauline Auvray <pauline.auvray@orange.com>
2024-10-15 10:49:54 +0200

    Review: Remove useless Image parameter

--- app/src/main/java/com/orange/ouds/app/ui/utilities/composable/DetailScreenHeader.kt
+++ app/src/main/java/com/orange/ouds/app/ui/utilities/composable/DetailScreenHeader.kt
@@ -21,7 +21,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
@@ -46,7 +45,6 @@ fun DetailScreenHeader(
             .fillMaxWidth()
             .height(186.dp)
             .background(OudsColorKeyToken.OnSurface.value), //TODO use BgEmphasizedPrimary token when available
-        alignment = Alignment.Center,
         contentScale = ContentScale.None
     )
 
