5d0f34a24404be8bb85e58127b979d7e915203f6
Pauline Auvray <pauline.auvray@orange.com>
2024-10-17 13:38:15 +0200

    Review: Move defaultIllustrationSize variable in the companion object of TokenProperty

--- app/src/main/java/com/orange/ouds/app/ui/tokens/TokenCategory.kt
+++ app/src/main/java/com/orange/ouds/app/ui/tokens/TokenCategory.kt
@@ -48,8 +48,6 @@ import com.orange.ouds.theme.tokens.semantic.OudsColorKeyToken
 
 val tokenCategories = TokenCategory::class.sealedSubclasses.mapNotNull { it.objectInstance }
 
-private val defaultIllustrationSize = 64.dp
-
 @Immutable
 sealed class TokenCategory(
     @StringRes val nameRes: Int,
