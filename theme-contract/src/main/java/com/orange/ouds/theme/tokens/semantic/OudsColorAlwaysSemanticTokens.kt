e54620ae2a1c64827cf34f79a5ce67c714d83e0b
Pauline Auvray <pauline.auvray@orange.com>
2024-10-23 14:30:33 +0200

    Split color semantic tokens file in several files

---
+++ theme-contract/src/main/java/com/orange/ouds/theme/tokens/semantic/OudsColorAlwaysSemanticTokens.kt
@@ -0,0 +1,47 @@
+/*
+ * Software Name: OUDS Android
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license,
+ * the text of which is available at https://opensource.org/license/MIT/
+ * or see the "LICENSE" file for more details.
+ *
+ * Software description: Android library of reusable graphical components
+ */
+
+package com.orange.ouds.theme.tokens.semantic
+
+import androidx.compose.ui.graphics.Color
+import com.orange.ouds.tokens.global.raw.ColorRawTokens
+
+data class OudsColorAlwaysSemanticTokens(
+    val alwaysAccentLight: Color = ColorRawTokens.colorFunctionalSun500,
+    val alwaysBlackLight: Color = ColorRawTokens.colorFunctionalBlack,
+    val alwaysInfoLight: Color = ColorRawTokens.colorFunctionalDodgerBlue500,
+    val alwaysNegativeLight: Color = ColorRawTokens.colorFunctionalScarlet600,
+    val alwaysOnAccentLight: Color = ColorRawTokens.colorFunctionalBlack,
+    val alwaysOnBlackLight: Color = ColorRawTokens.colorFunctionalWhite,
+    val alwaysOnInfoLight: Color = ColorRawTokens.colorFunctionalBlack,
+    val alwaysOnNegativeLight: Color = ColorRawTokens.colorFunctionalWhite,
+    val alwaysOnPositiveLight: Color = ColorRawTokens.colorFunctionalBlack,
+    val alwaysOnWarningLight: Color = ColorRawTokens.colorFunctionalBlack,
+    val alwaysOnWhiteLight: Color = ColorRawTokens.colorFunctionalBlack,
+    val alwaysPositiveLight: Color = ColorRawTokens.colorFunctionalMalachite500,
+    val alwaysWarningLight: Color = ColorRawTokens.colorFunctionalSun500,
+    val alwaysWhiteLight: Color = ColorRawTokens.colorFunctionalWhite,
+    val alwaysAccentDark: Color = ColorRawTokens.colorFunctionalSun500,
+    val alwaysBlackDark: Color = ColorRawTokens.colorFunctionalBlack,
+    val alwaysInfoDark: Color = ColorRawTokens.colorFunctionalDodgerBlue500,
+    val alwaysNegativeDark: Color = ColorRawTokens.colorFunctionalScarlet600,
+    val alwaysOnAccentDark: Color = ColorRawTokens.colorFunctionalDarkGray880,
+    val alwaysOnBlackDark: Color = ColorRawTokens.colorFunctionalLightGray160,
+    val alwaysOnInfoDark: Color = ColorRawTokens.colorFunctionalDarkGray880,
+    val alwaysOnNegativeDark: Color = ColorRawTokens.colorFunctionalLightGray160,
+    val alwaysOnPositiveDark: Color = ColorRawTokens.colorFunctionalDarkGray880,
+    val alwaysOnWarningDark: Color = ColorRawTokens.colorFunctionalDarkGray880,
+    val alwaysOnWhiteDark: Color = ColorRawTokens.colorFunctionalDarkGray880,
+    val alwaysPositiveDark: Color = ColorRawTokens.colorFunctionalMalachite500,
+    val alwaysWarningDark: Color = ColorRawTokens.colorFunctionalSun500,
+    val alwaysWhiteDark: Color = ColorRawTokens.colorFunctionalWhite,
+)
