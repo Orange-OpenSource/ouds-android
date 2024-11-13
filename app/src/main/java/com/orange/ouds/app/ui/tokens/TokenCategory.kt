2c0a1b9ef19a063d66f9536322f07d397b7dd5ba
Florent Maitre <florent.maitre@orange.com>
2024-11-13 14:40:05 +0100

    Add space tokens headers

--- app/src/main/java/com/orange/ouds/app/ui/tokens/TokenCategory.kt
+++ app/src/main/java/com/orange/ouds/app/ui/tokens/TokenCategory.kt
@@ -20,13 +20,13 @@ import com.orange.ouds.app.R
 val tokenCategories = TokenCategory::class.sealedSubclasses.mapNotNull { it.objectInstance }
 
 @Immutable
-sealed class TokenCategory(
+sealed class TokenCategory<T>(
     @StringRes val nameRes: Int,
     @DrawableRes val imageRes: Int,
     @StringRes val descriptionRes: Int,
-    val properties: List<TokenProperty> = emptyList(),
-    val subcategories: List<TokenCategory> = emptyList(),
-) {
+    val properties: List<TokenProperty<T>> = emptyList(),
+    val subcategories: List<TokenCategory<*>> = emptyList(),
+) where T : TokenCategory<T> {
 
     companion object {
         fun fromId(tokenId: Long) = tokenCategories.firstOrNull { token -> token.id == tokenId }
@@ -36,14 +36,14 @@ sealed class TokenCategory(
     val isSubcategory: Boolean
         get() = tokenCategories.any { it.subcategories.contains(this) }
 
-    data object Border : TokenCategory(
+    data object Border : TokenCategory<Border>(
         R.string.app_tokens_border_label,
         R.drawable.ic_border,
         R.string.app_tokens_border_description_text,
         listOf(TokenProperty.BorderWidth, TokenProperty.BorderRadius, TokenProperty.BorderStyle)
     )
 
-    data object Color : TokenCategory(
+    data object Color : TokenCategory<Color>(
         R.string.app_tokens_color_label,
         R.drawable.ic_palette,
         R.string.app_tokens_color_description_text,
@@ -60,13 +60,13 @@ sealed class TokenCategory(
         )
     )
 
-    data object Dimension : TokenCategory(
+    data object Dimension : TokenCategory<Dimension>(
         R.string.app_tokens_dimension_label,
         R.drawable.ic_dimension,
         R.string.app_tokens_dimension_description_text,
         subcategories = listOf(Space, Size)
     ) {
-        data object Space : TokenCategory(
+        data object Space : TokenCategory<Space>(
             R.string.app_tokens_dimension_space_label,
             R.drawable.ic_dimension,
             R.string.app_tokens_dimension_space_description_text,
@@ -87,7 +87,7 @@ sealed class TokenCategory(
             ),
         )
 
-        data object Size : TokenCategory(
+        data object Size : TokenCategory<Size>(
             R.string.app_tokens_dimension_size_label,
             R.drawable.ic_dimension,
             R.string.app_tokens_dimension_size_description_text,
@@ -95,28 +95,28 @@ sealed class TokenCategory(
         )
     }
 
-    data object Elevation : TokenCategory(
+    data object Elevation : TokenCategory<Elevation>(
         R.string.app_tokens_elevation_label,
         R.drawable.ic_layers,
         R.string.app_tokens_elevation_description_text,
         listOf(TokenProperty.Elevation)
     )
 
-    data object Grid : TokenCategory(
+    data object Grid : TokenCategory<Grid>(
         R.string.app_tokens_grid_label,
         R.drawable.ic_menu_grid,
         R.string.app_tokens_grid_description_text,
         listOf(TokenProperty.Grid)
     )
 
-    data object Opacity : TokenCategory(
+    data object Opacity : TokenCategory<Opacity>(
         R.string.app_tokens_opacity_label,
         R.drawable.ic_filter_effects,
         R.string.app_tokens_opacity_description_text,
         listOf(TokenProperty.Opacity)
     )
 
-    data object Typography : TokenCategory(
+    data object Typography : TokenCategory<Typography>(
         R.string.app_tokens_typography_label,
         R.drawable.ic_typography,
         R.string.app_tokens_typography_description_text,
