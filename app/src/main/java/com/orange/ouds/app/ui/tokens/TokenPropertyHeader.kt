895ebc6424cb05e57a13a2d61c93c6ad4607fb43
Pauline Auvray <pauline.auvray@orange.com>
2025-01-02 10:49:40 +0100

    Review: Fix SizeIconWithTextHeader preview text style

--- app/src/main/java/com/orange/ouds/app/ui/tokens/TokenPropertyHeader.kt
+++ app/src/main/java/com/orange/ouds/app/ui/tokens/TokenPropertyHeader.kt
@@ -31,6 +31,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -96,13 +97,17 @@ fun SizeIconWithTextHeader(
         )
 
         val tokenTypography = tokenName.split('.').take(2).joinToString(".")
-        val style = OudsTypographyKeyToken::class.getTokens()
-            .asOrNull<List<Token<TextStyle>>>()
-            ?.firstOrNull { typographyToken ->
-                typographyToken.name.replace(".Strong", "") == tokenTypography
-            }
-            ?.value?.invoke()
-            .orElse { LocalTextStyle.current }
+        val style = if (LocalInspectionMode.current) {
+            OudsTypographyKeyToken.Heading.Small.value
+        } else {
+            OudsTypographyKeyToken::class.getTokens()
+                .asOrNull<List<Token<TextStyle>>>()
+                ?.firstOrNull { typographyToken ->
+                    typographyToken.name.replace(".Strong", "") == tokenTypography
+                }
+                ?.value?.invoke()
+                .orElse { LocalTextStyle.current }
+        }
         Text(
             modifier = Modifier.weight(1f),
             text = tokenName.substringBeforeLast('.'),
