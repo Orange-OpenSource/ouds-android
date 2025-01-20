e6437efad4df3a70cd5760d2807a0f032ec7f2ab
Pauline Auvray <pauline.auvray@orange.com>
2025-01-20 11:19:17 +0100

    Update orange country colors

--- theme-orange-country/src/main/java/com/orange/ouds/theme/orangecountry/OrangeCountryTheme.kt
+++ theme-orange-country/src/main/java/com/orange/ouds/theme/orangecountry/OrangeCountryTheme.kt
@@ -14,26 +14,52 @@ package com.orange.ouds.theme.orangecountry
 
 import androidx.compose.ui.graphics.Color
 import com.orange.ouds.theme.orange.OrangeTheme
+import com.orange.ouds.theme.orange.tokens.semantic.OrangeColorActionSemanticTokens
 import com.orange.ouds.theme.orange.tokens.semantic.OrangeColorBgSemanticTokens
+import com.orange.ouds.theme.orange.tokens.semantic.OrangeColorBorderSemanticTokens
+import com.orange.ouds.theme.orange.tokens.semantic.OrangeColorContentSemanticTokens
+import com.orange.ouds.theme.orange.tokens.semantic.OrangeColorRepositorySemanticTokens
 import com.orange.ouds.theme.orange.tokens.semantic.OrangeColorSemanticTokens
+import com.orange.ouds.theme.orange.tokens.semantic.OrangeColorSurfaceSemanticTokens
 import com.orange.ouds.theme.tokens.OudsBorderKeyToken
 import com.orange.ouds.theme.tokens.OudsColorKeyToken
 import com.orange.ouds.theme.tokens.components.OudsButtonTokens
 import com.orange.ouds.theme.tokens.components.OudsComponentsTokens
 import com.orange.ouds.theme.tokens.semantic.OudsColorSemanticTokens
+import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class OrangeCountryTheme : OrangeTheme() {
+    @IgnoredOnParcel
+    private val colorGreen500 = Color(0xFF34D349)
+    @IgnoredOnParcel
+    private val colorGreen600 = Color(0xFF069D1A)
 
     override val name: String
         get() = "Orange country"
 
     override val colorTokens: OudsColorSemanticTokens
         get() = OrangeColorSemanticTokens(
-            backgroundColorTokens = OrangeColorBgSemanticTokens(
-                bgPrimaryLight = Color(0xFF34D349),
-                bgPrimaryDark = Color(0xFF069D1A)
+            actionColorTokens = OrangeColorActionSemanticTokens(
+                actionLoadingLight = colorGreen500,
+                actionPressedLight = colorGreen500,
+                actionSelectedLight = colorGreen500,
+                actionLoadingDark = colorGreen600,
+                actionPressedDark = colorGreen600,
+                actionSelectedDark = colorGreen600
+            ),
+            borderColorTokens = OrangeColorBorderSemanticTokens(
+                borderBrandPrimaryLight = colorGreen600,
+                borderBrandPrimaryDark = colorGreen500
+            ),
+            contentColorTokens = OrangeColorContentSemanticTokens(
+                contentBrandPrimaryLight = colorGreen600,
+                contentBrandPrimaryDark = colorGreen500
+            ),
+            surfaceColorTokens = OrangeColorSurfaceSemanticTokens(
+                surfaceBrandPrimaryLight = colorGreen500,
+                surfaceBrandPrimaryDark = colorGreen500
             )
         )
 
