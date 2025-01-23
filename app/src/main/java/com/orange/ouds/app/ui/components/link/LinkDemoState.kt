16c013185f43c6ab6e88750e9bc1d6ae4809cdde
Pauline Auvray <pauline.auvray@orange.com>
2025-01-23 16:10:49 +0100

    Review: Change customization labels for back and next

--- app/src/main/java/com/orange/ouds/app/ui/components/link/LinkDemoState.kt
+++ app/src/main/java/com/orange/ouds/app/ui/components/link/LinkDemoState.kt
@@ -78,7 +78,7 @@ class LinkDemoState(
     enum class Layout(@StringRes val labelRes: Int) {
         TextOnly(R.string.app_components_common_textOnlyLayout_label),
         IconAndText(R.string.app_components_common_iconAndTextLayout_label),
-        ArrowBack(R.string.app_components_link_arrowBack_label),
-        ArrowNext(R.string.app_components_link_arrowNext_label)
+        ArrowBack(R.string.app_components_link_back_label),
+        ArrowNext(R.string.app_components_link_next_label)
     }
 }
