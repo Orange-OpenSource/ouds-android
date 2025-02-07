b4ba1197ed6092d73df9d472e37abefac4b695c2
Pauline Auvray <pauline.auvray@orange.com>
2025-02-07 10:53:12 +0100

    Implement standalone checkbox component

---
+++ core/src/main/java/com/orange/ouds/core/component/samples/OudsCheckboxSamples.kt
@@ -0,0 +1,46 @@
+/*
+ * Software Name: OUDS Android
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license,
+ * the text of which is available at https://opensource.org/license/MIT/
+ * or see the "LICENSE" file for more details.
+ *
+ * Software description: Android library of reusable graphical components
+ */
+
+package com.orange.ouds.core.component.samples
+
+import OudsCheckbox
+import OudsTriStateCheckbox
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.state.ToggleableState
+
+@Composable
+internal fun OudsCheckboxSample() {
+    OudsCheckbox(
+        checked = false,
+        onCheckedChange = { _ -> /* Do something! */ }
+    )
+}
+
+@Composable
+internal fun OudsTriStateCheckboxSample() {
+    var toggleableState by remember { mutableStateOf(ToggleableState.Off) }
+
+    OudsTriStateCheckbox(
+        state = toggleableState,
+        onClick = {
+            toggleableState = when (toggleableState) {
+                ToggleableState.On -> ToggleableState.Off
+                ToggleableState.Off -> ToggleableState.Indeterminate
+                ToggleableState.Indeterminate -> ToggleableState.On
+            }
+        }
+    )
+}
