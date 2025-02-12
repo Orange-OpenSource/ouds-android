43f0191cb4520151be0945dd59bb569a09fe97bf
Florent Maitre <florent.maitre@orange.com>
2025-02-12 12:55:32 +0100

    Add code snippet for OudsButton and OudsLink

---
+++ app/src/main/java/com/orange/ouds/app/ui/components/ComponentCode.kt
@@ -0,0 +1,28 @@
+/*
+ * Software Name: OUDS Android
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license,
+ * the text of which is available at https://opensource.org/license/MIT/
+ * or see the "LICENSE" file for more details.
+ *
+ * Software description: Android library of reusable graphical components
+ */
+
+package com.orange.ouds.app.ui.components
+
+import com.orange.ouds.app.ui.utilities.Code
+import com.orange.ouds.core.component.OudsColoredBox
+
+fun Code.Builder.coloredBoxCall(onColoredBox: Boolean, content: Code.Builder.() -> Unit) {
+    if (onColoredBox) {
+        functionCall("OudsColoredBox") {
+            trailingLambda = true
+            typedArgument("color", OudsColoredBox.Color.BrandPrimary)
+            lambdaArgument("content", content)
+        }
+    } else {
+        content()
+    }
+}
