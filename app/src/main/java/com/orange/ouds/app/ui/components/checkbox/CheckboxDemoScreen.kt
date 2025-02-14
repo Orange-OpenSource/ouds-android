fe0aab3b442a11535fb6bd4452de0ed82d591dd2
Pauline Auvray <pauline.auvray@orange.com>
2025-02-14 15:42:23 +0100

    Review: Add vertical scroll to the column

--- app/src/main/java/com/orange/ouds/app/ui/components/checkbox/CheckboxDemoScreen.kt
+++ app/src/main/java/com/orange/ouds/app/ui/components/checkbox/CheckboxDemoScreen.kt
@@ -17,6 +17,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
@@ -61,7 +63,7 @@ fun CheckboxDemoScreen() = DemoScreen(rememberCheckboxDemoState()) {
             )
         }
     ) {
-        Column(modifier = Modifier.fillMaxWidth()) {
+        Column(modifier = Modifier.fillMaxWidth().verticalScroll(rememberScrollState())) {
             DetailScreenDescription(
                 modifier = Modifier.padding(all = OudsTheme.spaces.fixed.medium),
                 descriptionRes = Component.Checkbox.descriptionRes
