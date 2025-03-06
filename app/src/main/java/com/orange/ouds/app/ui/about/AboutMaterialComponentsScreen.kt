f5e409669cda313030e6d2b2dfb9df2608855f92
Florent Maitre <florent.maitre@orange.com>
2025-03-06 11:16:07 +0100

    fix: customization bottom sheet bugs (#349)
    * Fix a bug where customization bottom sheet expands on device orientation change
    
    * Fix a bug where screen content is not visible around the bottom sheet in landscape
    
    * Fix a bug where bottom sheet content is visible underneath the system navigation bar

--- app/src/main/java/com/orange/ouds/app/ui/about/AboutMaterialComponentsScreen.kt
+++ app/src/main/java/com/orange/ouds/app/ui/about/AboutMaterialComponentsScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -758,6 +759,7 @@ private fun BottomSheetScaffoldSample(
         Box(
             modifier = Modifier
                 .fillMaxSize()
+                .consumeWindowInsets(innerPadding)
                 .padding(innerPadding)
         ) {
             content()
