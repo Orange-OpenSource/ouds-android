7d4dcd9283e2a3a38233623c0a1fc48bca9c493c
Pauline Auvray <pauline.auvray@orange.com>
2025-03-20 16:51:23 +0100

    Review: Fix crash on Florent's device

--- core/src/main/java/com/orange/ouds/core/extensions/ContextExt.kt
+++ core/src/main/java/com/orange/ouds/core/extensions/ContextExt.kt
@@ -12,15 +12,27 @@
 
 package com.orange.ouds.core.extensions
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.accessibility.AccessibilityManager
 import com.orange.ouds.foundation.extensions.orElse
+import com.orange.ouds.foundation.extensions.tryOrNull
 
 /**
  * @return true is the high contrast mode is enabled on the device, false otherwise
  */
+@SuppressLint("DiscouragedPrivateApi", "PrivateApi")
 internal fun Context.isHighContrastModeEnabled(): Boolean {
-    val accessibilityManager = this.getSystemService(Context.ACCESSIBILITY_SERVICE) as AccessibilityManager
-    // Workaround to use 'isHighTextContrastEnabled()' because it is hidden with a @UnsupportedAppUsage annotation
-    return (accessibilityManager.javaClass.getMethod("isHighTextContrastEnabled").invoke(accessibilityManager) as? Boolean).orElse { false }
+    val accessibilityManager = getSystemService(Context.ACCESSIBILITY_SERVICE) as AccessibilityManager
+
+    return with(accessibilityManager) {
+        // Workaround to access mIsHighTextContrastEnabled field because it is hidden with a @UnsupportedAppUsage annotation
+        // On some OS versions this field is named mIsHighContrastTextEnabled
+        // TODO Use high text contrast APIs once API level 36 is available
+        tryOrNull { javaClass.getDeclaredField("mIsHighTextContrastEnabled") }
+            .orElse { tryOrNull { javaClass.getDeclaredField("mIsHighContrastTextEnabled") } }
+            ?.also { it.isAccessible = true }
+            ?.getBoolean(this)
+            .orElse { false }
+    }
 }
