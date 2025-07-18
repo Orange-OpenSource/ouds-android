b91447440e62d0b37aaa93501a6b9ad46010fe19
Pauline Auvray <pauline.auvray@orange.com>
2025-07-18 09:36:33 +0200

    Review: Remove useless imports

--- app/src/main/java/com/orange/ouds/app/ui/components/badge/BadgeDemoScreen.kt
+++ app/src/main/java/com/orange/ouds/app/ui/components/badge/BadgeDemoScreen.kt
@@ -17,8 +17,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
