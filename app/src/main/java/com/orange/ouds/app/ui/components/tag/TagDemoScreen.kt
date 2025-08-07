c18e9b320377dd9643cbcd32b95310e6e644947d
Pauline Auvray <pauline.auvray@orange.com>
2025-08-07 16:22:04 +0200

    Fix code snippet for loading parameter

--- app/src/main/java/com/orange/ouds/app/ui/components/tag/TagDemoScreen.kt
+++ app/src/main/java/com/orange/ouds/app/ui/components/tag/TagDemoScreen.kt
@@ -167,7 +167,11 @@ private fun Code.Builder.tagDemoCodeSnippet(state: TagDemoState) {
             typedArgument("shape", shape)
             typedArgument("size", size)
             typedArgument("status", status)
-            typedArgument("loading", loading)
+            if (loading) {
+                constructorCallArgument<OudsTag.Loading>("loading") {
+                    typedArgument("progress", null)
+                }
+            }
         }
     }
 }
