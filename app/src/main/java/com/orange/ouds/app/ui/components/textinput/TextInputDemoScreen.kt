57f0131656bc5cec614c2122200da213c38d992c
Pauline Auvray <pauline.auvray@orange.com>
2025-09-10 16:17:31 +0200

    Add text input component to demo app

---
+++ app/src/main/java/com/orange/ouds/app/ui/components/textinput/TextInputDemoScreen.kt
@@ -0,0 +1,159 @@
+/*
+ * Software Name: OUDS Android
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license,
+ * the text of which is available at https://opensource.org/license/MIT/
+ * or see the "LICENSE" file for more details.
+ *
+ * Software description: Android library of reusable graphical components
+ */
+
+package com.orange.ouds.app.ui.components.textinput
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import com.orange.ouds.app.R
+import com.orange.ouds.app.ui.components.Component
+import com.orange.ouds.app.ui.utilities.Code
+import com.orange.ouds.app.ui.utilities.composable.CustomizationSwitchItem
+import com.orange.ouds.app.ui.utilities.composable.CustomizationTextField
+import com.orange.ouds.app.ui.utilities.composable.DemoScreen
+import com.orange.ouds.core.component.OudsTextInput
+import com.orange.ouds.core.theme.OudsTheme
+import com.orange.ouds.core.utilities.OudsPreview
+import com.orange.ouds.theme.OudsVersion
+
+@Composable
+fun TextInputDemoScreen() {
+    val state = rememberTextInputDemoState()
+    DemoScreen(
+        description = stringResource(id = Component.TextInput.descriptionRes),
+        bottomSheetContent = { TextInputDemoBottomSheetContent(state = state) },
+        codeSnippet = { textInputDemoCodeSnippet(state = state) },
+        demoContent = { TextInputDemoContent(state = state) },
+        version = OudsVersion.Component.TextInput
+    )
+}
+
+@Composable
+private fun TextInputDemoBottomSheetContent(state: TextInputDemoState) {
+    with(state) {
+        CustomizationSwitchItem(
+            label = stringResource(R.string.app_components_textInput_outlined_label),
+            checked = outlined,
+            onCheckedChange = { outlined = it },
+        )
+        CustomizationSwitchItem(
+            label = stringResource(R.string.app_components_textInput_leadingIcon_label),
+            checked = leadingIcon,
+            onCheckedChange = { leadingIcon = it },
+        )
+        CustomizationSwitchItem(
+            label = stringResource(R.string.app_components_textInput_trailingIcon_label),
+            checked = trailingIcon,
+            onCheckedChange = { trailingIcon = it },
+        )
+        CustomizationSwitchItem(
+            label = stringResource(R.string.app_components_common_loader_label),
+            checked = hasLoader,
+            onCheckedChange = { hasLoader = it },
+            enabled = loaderSwitchEnabled
+        )
+        CustomizationSwitchItem(
+            label = stringResource(R.string.app_common_enabled_label),
+            checked = enabled,
+            onCheckedChange = { enabled = it },
+            enabled = enabledSwitchEnabled
+        )
+        CustomizationSwitchItem(
+            label = stringResource(R.string.app_components_common_readOnly_label),
+            checked = readOnly,
+            onCheckedChange = { readOnly = it },
+            enabled = readOnlySwitchEnabled
+        )
+        CustomizationSwitchItem(
+            label = stringResource(R.string.app_components_common_error_label),
+            checked = error,
+            onCheckedChange = { error = it },
+            enabled = errorSwitchEnabled
+        )
+        CustomizationTextField(
+            modifier = Modifier.padding(top = OudsTheme.spaces.fixed.medium),
+            label = stringResource(R.string.app_components_common_label_label),
+            value = label,
+            onValueChange = { value -> label = value }
+        )
+        CustomizationTextField(
+            modifier = Modifier.padding(top = OudsTheme.spaces.fixed.medium),
+            label = stringResource(R.string.app_components_textInput_placeholder_label),
+            value = placeholder,
+            onValueChange = { value -> placeholder = value }
+        )
+        CustomizationTextField(
+            modifier = Modifier.padding(top = OudsTheme.spaces.fixed.medium),
+            label = stringResource(R.string.app_components_textInput_prefix_label),
+            value = prefix,
+            onValueChange = { value -> prefix = value }
+        )
+        CustomizationTextField(
+            modifier = Modifier.padding(top = OudsTheme.spaces.fixed.medium),
+            label = stringResource(R.string.app_components_textInput_suffix_label),
+            value = suffix,
+            onValueChange = { value -> suffix = value }
+        )
+        CustomizationTextField(
+            modifier = Modifier.padding(top = OudsTheme.spaces.fixed.medium),
+            label = stringResource(R.string.app_components_textInput_helperText_label),
+            value = helperText,
+            onValueChange = { value -> helperText = value }
+        )
+        CustomizationTextField(
+            modifier = Modifier.padding(top = OudsTheme.spaces.fixed.medium),
+            label = stringResource(R.string.app_components_textInput_helperLink_label),
+            value = helperLink,
+            onValueChange = { value -> helperLink = value }
+        )
+    }
+}
+
+@Composable
+private fun TextInputDemoContent(state: TextInputDemoState) {
+    with(state) {
+        OudsTextInput(
+            value = value,
+            onValueChange = { value = it },
+            label = label.ifEmpty { null },
+            placeholder = placeholder.ifEmpty { null },
+            outlined = outlined,
+            leadingIcon = if (leadingIcon) OudsTextInput.LeadingIcon(painterResource(id = R.drawable.ic_heart), contentDescription = "") else null,
+            trailingIconButton = if (trailingIcon) OudsTextInput.TrailingIconButton(
+                painterResource(id = R.drawable.ic_heart),
+                contentDescription = "",
+                onClick = { }) else null,
+            loader = if (hasLoader) OudsTextInput.Loader(null) else null,
+            enabled = enabled,
+            readOnly = readOnly,
+            error = error,
+            prefix = prefix.ifEmpty { null },
+            suffix = suffix.ifEmpty { null },
+            helperText = helperText.ifEmpty { null },
+            helperLink = if (helperLink.isNotEmpty()) OudsTextInput.HelperLink(text = helperLink, onClick = { }) else null
+        )
+    }
+}
+
+private fun Code.Builder.textInputDemoCodeSnippet(state: TextInputDemoState) {
+//TODO
+}
+
+@PreviewLightDark
+@Composable
+private fun PreviewTextInputDemoScreen() = OudsPreview {
+    TextInputDemoScreen()
+}
