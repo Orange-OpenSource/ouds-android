da3b8b513b6480e16cf898f9cb3c74f008256cad
Pauline Auvray <pauline.auvray@orange.com>
2025-11-10 11:40:32 +0100

    Update versions in documentation

--- core/src/main/java/com/orange/ouds/core/component/OudsRadioButton.kt
+++ core/src/main/java/com/orange/ouds/core/component/OudsRadioButton.kt
@@ -64,7 +64,7 @@ import com.orange.ouds.theme.OudsThemeContract
  *
  * > Design guidelines: [unified-design-system.orange.com](https://unified-design-system.orange.com/472794e18/p/90c467-radio-button)
  *
- * > Design version: 1.3.0
+ * > Design version: 1.4.0
  *
  * @see [OudsRadioButtonItem] If you want to use a radio button with an associated label and other optional elements.
  *
