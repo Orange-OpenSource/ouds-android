e85842bdd18be2eaa2be0a2f8aaa1b2921385e47
Pauline Auvray <pauline.auvray@orange.com>
2026-01-06 16:43:29 +0100

    Update OudsDivider code connect

--- core/src/main/java/com/orange/ouds/core/component/figma/OudsDividerConnection.kt
+++ core/src/main/java/com/orange/ouds/core/component/figma/OudsDividerConnection.kt
@@ -18,22 +18,34 @@ import com.figma.code.connect.FigmaVariant
 import com.orange.ouds.core.component.OudsHorizontalDivider
 import com.orange.ouds.core.component.OudsVerticalDivider
 
+/**
+ * @suppress
+ */
 @FigmaConnect(url = "<FIGMA_COMPONENTS_DIVIDER>")
 @FigmaVariant("Orientation", "Horizontal")
 class OudsDividerConnection {
 
     @Composable
     fun OudsHorizontalDividerExample() {
+        // Divider color can be changed by setting the color parameter
+        // to a different OudsDividerColor enum value
+        // OudsHorizontalDivider(color = OudsDividerColor.Emphasized)
         OudsHorizontalDivider()
     }
 }
 
+/**
+ * @suppress
+ */
 @FigmaConnect(url = "<FIGMA_COMPONENTS_DIVIDER>")
 @FigmaVariant("Orientation", "Vertical")
 class OudsVerticalDividerConnection {
 
     @Composable
     fun OudsVerticalDividerExample() {
+        // Divider color can be changed by setting the color parameter
+        // to a different OudsDividerColor enum value
+        // OudsVerticalDivider(color = OudsDividerColor.Emphasized)
         OudsVerticalDivider()
     }
 }
