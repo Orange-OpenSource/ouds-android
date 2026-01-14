1216c1ce01e9653a37f68939fae6145740e5be93
Pauline Auvray <pauline.auvray@orange.com>
2026-01-14 09:36:11 +0100

    Review: Remove useless material3.android dependency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -46,7 +46,6 @@ dependencies {
     implementation(libs.androidx.compose.material.icons.core)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material3.adaptive)
-    implementation(libs.androidx.compose.material3.android)
     debugImplementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.constraintlayout.compose)
