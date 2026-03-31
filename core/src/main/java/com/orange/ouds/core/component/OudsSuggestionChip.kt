5a00101b25ca95187a617f84b77641c7eb664d1f
Pauline Auvray <pauline.auvray@orange.com>
2026-03-31 17:38:03 +0200

    Add high contrast mode previews and snapshots

--- core/src/main/java/com/orange/ouds/core/component/OudsSuggestionChip.kt
+++ core/src/main/java/com/orange/ouds/core/component/OudsSuggestionChip.kt
@@ -12,6 +12,7 @@
 
 package com.orange.ouds.core.component
 
+import android.R.attr.label
 import androidx.compose.foundation.interaction.Interaction
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -20,6 +21,7 @@ import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import com.orange.ouds.core.component.OudsFilterChip
 import com.orange.ouds.core.utilities.OudsPreview
 import com.orange.ouds.core.utilities.OudsPreviewLightDark
 import com.orange.ouds.core.utilities.PreviewEnumEntries
@@ -204,6 +206,24 @@ internal fun PreviewOudsSuggestionChip(
             OudsSuggestionChip(nullableIcon = icon, nullableLabel = label, onClick = {})
         }
     }
+}
+
+@OudsPreviewLightDark
+@Composable
+@Suppress("PreviewShouldNotBeCalledRecursively")
+private fun PreviewOudsSuggestionChipHighContrastModeEnabled() {
+    PreviewOudsSuggestionChipHighContrastModeEnabled(
+        theme = getPreviewTheme(),
+        darkThemeEnabled = isSystemInDarkTheme()
+    )
+}
+
+@Composable
+internal fun PreviewOudsSuggestionChipHighContrastModeEnabled(
+    theme: OudsThemeContract,
+    darkThemeEnabled: Boolean
+) = OudsPreview(theme = theme, darkThemeEnabled = darkThemeEnabled, highContrastModeEnabled = true) {
+    OudsSuggestionChip(nullableIcon = null, nullableLabel = "Label", onClick = {})
 }
 
 internal data class OudsSuggestionChipPreviewParameter(
