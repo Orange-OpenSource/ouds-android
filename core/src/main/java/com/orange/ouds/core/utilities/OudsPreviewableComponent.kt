84a013b67c3f381f3dedf07d003d51e1941eedbb
Pauline Auvray <pauline.auvray@orange.com>
2026-04-08 11:57:55 +0200

    Review: Remove unnecessary annotation

--- core/src/main/java/com/orange/ouds/core/utilities/OudsPreviewableComponent.kt
+++ core/src/main/java/com/orange/ouds/core/utilities/OudsPreviewableComponent.kt
@@ -851,7 +851,6 @@ interface OudsPreviewableComponent {
 
             @Composable
             override fun Preview(theme: OudsThemeContract, darkThemeEnabled: Boolean, highContrastModeEnabled: Boolean, parameter: Any?) {
-                @Suppress("UNCHECKED_CAST")
                 PreviewOudsSuggestionChipHighContrastModeEnabled(
                     theme = theme,
                     darkThemeEnabled = darkThemeEnabled
