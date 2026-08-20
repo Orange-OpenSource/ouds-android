0acd1a4bd4513388c6df123e14e1beba68eadd8e
Pauline Auvray <pauline.auvray@orange.com>
2026-08-20 17:44:34 +0200

    Add missing information in kdoc

--- core/src/main/java/com/orange/ouds/core/component/OudsDisplayText.kt
+++ core/src/main/java/com/orange/ouds/core/component/OudsDisplayText.kt
@@ -26,16 +26,21 @@ import com.orange.ouds.core.utilities.PreviewPaddingDefault
 import com.orange.ouds.core.utilities.getPreviewTheme
 import com.orange.ouds.theme.OudsThemeContract
 
+// TODO Add design guideline link when available
 /**
  * Display styles are intended for high-impact content such as landing pages, marketing campaigns, and key messages. Their large type sizes help capture
  * attention and establish strong visual emphasis.
  * Variants automatically adapt across breakpoints to maintain a consistent visual hierarchy on every screen size.
  * Use them sparingly to preserve their impact and effectiveness.
  *
+ * > Design name: Display
+ *
+ * > Design version: 1.0.0
+ *
  * @param text Text to display.
  * @param modifier [Modifier] applied to the display text.
  * @param size Size of the display text.
- * 
+ *
  * @sample com.orange.ouds.core.component.samples.OudsDisplayTextSample
  */
 @Composable
