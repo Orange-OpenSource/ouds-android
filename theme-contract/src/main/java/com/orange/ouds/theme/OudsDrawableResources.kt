89f4cc74d38ed76eb8494e452994f7ea15429e5d
Pauline Auvray <pauline.auvray@orange.com>
2026-08-20 17:44:10 +0200

    Add new resources to NOTICE.txt

--- theme-contract/src/main/java/com/orange/ouds/theme/OudsDrawableResources.kt
+++ theme-contract/src/main/java/com/orange/ouds/theme/OudsDrawableResources.kt
@@ -200,6 +200,9 @@ interface OudsDrawableResources {
         }
     }
 
+    /**
+     * Note: Resources in [Other] interface are not coming from the OUDS icon kit.
+     */
     interface Other {
         @get:DrawableRes
         val headingTextMarker: Int?
