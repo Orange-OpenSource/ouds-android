c7a2d3661d1a9457092315c1a025a11872932ff1
Florent Maitre <florent.maitre@orange.com>
2026-09-09 18:02:36 +0200

    Add helper text to demos

--- app/src/main/java/com/orange/ouds/app/ui/components/progressindicator/LinearProgressIndicatorDemoScreen.kt
+++ app/src/main/java/com/orange/ouds/app/ui/components/progressindicator/LinearProgressIndicatorDemoScreen.kt
@@ -13,16 +13,21 @@
 package com.orange.ouds.app.ui.components.progressindicator
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import com.orange.ouds.app.R
 import com.orange.ouds.app.ui.components.Variant
 import com.orange.ouds.app.ui.utilities.Code
 import com.orange.ouds.app.ui.utilities.composable.AppPreview
+import com.orange.ouds.app.ui.utilities.composable.CustomizationFilterChip
+import com.orange.ouds.app.ui.utilities.composable.CustomizationFilterChips
 import com.orange.ouds.app.ui.utilities.composable.CustomizationSwitchItem
-import com.orange.ouds.app.ui.utilities.composable.CustomizationTextInput
 import com.orange.ouds.app.ui.utilities.composable.DemoScreen
+import com.orange.ouds.core.component.OudsDeterminateLinearProgressIndicatorHelperText
+import com.orange.ouds.core.component.OudsIndeterminateLinearProgressIndicatorHelperText
 import com.orange.ouds.core.component.OudsLinearProgressIndicator
+import com.orange.ouds.foundation.extensions.toSentenceCase
 import com.orange.ouds.theme.OudsVersion
 
 @Composable
@@ -42,17 +47,26 @@ fun LinearProgressIndicatorDemoScreen() {
 private fun LinearProgressIndicatorDemoBottomSheetContent(state: LinearProgressIndicatorDemoState) {
     with(state) {
         ProgressIndicatorDemoBottomSheetContent(state = state)
+        val helperTextAlignments = LinearProgressIndicatorDemoState.HelperTextAlignment.entries
+        CustomizationFilterChips(
+            applyTopPadding = false,
+            label = stringResource(R.string.app_components_progressIndicator_helperTextProgressAlignment_tech),
+            chips = helperTextAlignments.map { CustomizationFilterChip(it.name.toSentenceCase(), helperTextProgressAlignmentEnabled) },
+            selectedChipIndex = helperTextAlignments.indexOf(helperTextProgressAlignment),
+            onSelectionChange = { index -> helperTextProgressAlignment = helperTextAlignments[index] }
+        )
+        CustomizationFilterChips(
+            applyTopPadding = false,
+            label = stringResource(R.string.app_components_progressIndicator_helperTextLabelAlignment_tech),
+            chips = helperTextAlignments.map { CustomizationFilterChip(it.name.toSentenceCase(), helperTextLabelAlignmentEnabled) },
+            selectedChipIndex = helperTextAlignments.indexOf(helperTextLabelAlignment),
+            onSelectionChange = { index -> helperTextLabelAlignment = helperTextAlignments[index] }
+        )
         CustomizationSwitchItem(
             label = stringResource(R.string.app_components_progressIndicator_linearProgressIndicator_stopIndicator_tech),
             checked = stopIndicator,
             onCheckedChange = { stopIndicator = it },
             enabled = stopIndicatorSwitchEnabled
-        )
-        CustomizationTextInput(
-            applyTopPadding = true,
-            label = stringResource(R.string.app_components_common_helperText_tech),
-            value = helperText.orEmpty(),
-            onValueChange = { value -> helperText = value }
         )
     }
 }
@@ -64,22 +78,50 @@ private fun LinearProgressIndicatorDemoContent(state: LinearProgressIndicatorDem
     with(state) {
         when (type) {
             ProgressIndicatorDemoState.Type.Determinate -> {
-                OudsLinearProgressIndicator(
-                    progress = { progress },
-                    status = status,
-                    track = track,
-                    stopIndicator = stopIndicator,
-                    helperText = helperText,
-                    gapSize = gapSize
-                )
+                if (helperTextProgress || !helperTextLabel.isNullOrBlank()) {
+                    val linearProgressIndicatorHelperText = OudsDeterminateLinearProgressIndicatorHelperText(
+                        progress = helperTextProgress,
+                        label = helperTextLabel,
+                        progressAlignment = helperTextProgressAlignment.toHorizontalAlignment(),
+                        labelAlignment = helperTextLabelAlignment.toHorizontalAlignment()
+                    )
+                    OudsLinearProgressIndicator(
+                        progress = { progress },
+                        status = status,
+                        track = track,
+                        stopIndicator = stopIndicator,
+                        helperText = linearProgressIndicatorHelperText,
+                        gapSize = gapSize
+                    )
+                } else {
+                    OudsLinearProgressIndicator(
+                        progress = { progress },
+                        status = status,
+                        track = track,
+                        stopIndicator = stopIndicator,
+                        gapSize = gapSize
+                    )
+                }
             }
             ProgressIndicatorDemoState.Type.Indeterminate -> {
-                OudsLinearProgressIndicator(
-                    status = status,
-                    track = track,
-                    helperText = helperText,
-                    gapSize = gapSize
-                )
+                if (!helperTextLabel.isNullOrBlank()) {
+                    val linearProgressIndicatorHelperText = OudsIndeterminateLinearProgressIndicatorHelperText(
+                        label = helperTextLabel.orEmpty(),
+                        alignment = helperTextLabelAlignment.toHorizontalAlignment()
+                    )
+                    OudsLinearProgressIndicator(
+                        status = status,
+                        track = track,
+                        helperText = linearProgressIndicatorHelperText,
+                        gapSize = gapSize
+                    )
+                } else {
+                    OudsLinearProgressIndicator(
+                        status = status,
+                        track = track,
+                        gapSize = gapSize
+                    )
+                }
             }
         }
     }
@@ -87,10 +129,31 @@ private fun LinearProgressIndicatorDemoContent(state: LinearProgressIndicatorDem
 
 private fun Code.Builder.linearProgressIndicatorDemoCodeSnippet(state: LinearProgressIndicatorDemoState) {
     progressIndicatorAnimationInitialization(state)
-
+    
     functionCall("OudsLinearProgressIndicator") {
         progressIndicatorArguments(state = state)
-        state.helperText?.let { typedArgument("helperText", it) }
+        with(state) {
+            when (type) {
+                ProgressIndicatorDemoState.Type.Determinate -> {
+                    if (helperTextProgress || !helperTextLabel.isNullOrBlank()) {
+                        constructorCallArgument<OudsDeterminateLinearProgressIndicatorHelperText>("helperText") {
+                            typedArgument("progress", helperTextProgress)
+                            typedArgument("label", helperTextLabel)
+                            rawArgument("progressAlignment", "${Alignment::class.simpleName}.${helperTextProgressAlignment.name}")
+                            rawArgument("labelAlignment", "${Alignment::class.simpleName}.${helperTextLabelAlignment.name}")
+                        }
+                    }
+                }
+                ProgressIndicatorDemoState.Type.Indeterminate -> {
+                    if (!helperTextLabel.isNullOrBlank()) {
+                        constructorCallArgument<OudsIndeterminateLinearProgressIndicatorHelperText>("helperText") {
+                            typedArgument("label", helperTextLabel)
+                            rawArgument("labelAlignment", "${Alignment::class.simpleName}.${helperTextLabelAlignment.name}")
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
