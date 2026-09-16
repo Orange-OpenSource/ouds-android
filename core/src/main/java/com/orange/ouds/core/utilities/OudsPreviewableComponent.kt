54b4ffe4b1ed72760b0458f2b8ce3e88408d5970
Florent Maitre <florent.maitre@orange.com>
2026-09-16 17:39:08 +0200

    Add gradient to previews

--- core/src/main/java/com/orange/ouds/core/utilities/OudsPreviewableComponent.kt
+++ core/src/main/java/com/orange/ouds/core/utilities/OudsPreviewableComponent.kt
@@ -73,6 +73,7 @@ import com.orange.ouds.core.component.OudsRadioButtonItemPreviewParameter
 import com.orange.ouds.core.component.OudsRadioButtonItemPreviewParameterProvider
 import com.orange.ouds.core.component.OudsRadioButtonPreviewParameter
 import com.orange.ouds.core.component.OudsRadioButtonPreviewParameterProvider
+import com.orange.ouds.core.component.OudsSkeletonPreviewParameter
 import com.orange.ouds.core.component.OudsSkeletonPreviewParameterProvider
 import com.orange.ouds.core.component.OudsSmallCardItemPreviewParameterProvider
 import com.orange.ouds.core.component.OudsSmallListItemLeading
@@ -1623,7 +1624,7 @@ interface OudsPreviewableComponent {
             PreviewOudsSkeleton(
                 theme = theme,
                 darkThemeEnabled = darkThemeEnabled,
-                securityMargin = parameter as Boolean
+                parameter = parameter as OudsSkeletonPreviewParameter
             )
         }
     }
