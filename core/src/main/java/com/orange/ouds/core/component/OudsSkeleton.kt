76db51bfc5cb8b6291b343c7e960aa6c871011e9
Florent Maitre <florent.maitre@orange.com>
2026-09-22 15:03:06 +0200

    Opt in RestrictedOudsApi annotation in OudsSkeleton

--- core/src/main/java/com/orange/ouds/core/component/OudsSkeleton.kt
+++ core/src/main/java/com/orange/ouds/core/component/OudsSkeleton.kt
@@ -10,6 +10,8 @@
  * Software description: Android library of reusable graphical components 
  */
 
+@file:OptIn(RestrictedOudsApi::class)
+
 package com.orange.ouds.core.component
 
 import androidx.compose.animation.core.CubicBezierEasing
@@ -54,6 +56,7 @@ import com.orange.ouds.core.theme.OudsTheme
 import com.orange.ouds.core.utilities.OudsPreview
 import com.orange.ouds.core.utilities.OudsPreviewLightDark
 import com.orange.ouds.core.utilities.getPreviewTheme
+import com.orange.ouds.foundation.RestrictedOudsApi
 import com.orange.ouds.foundation.utilities.BasicPreviewParameterProvider
 import com.orange.ouds.theme.OudsThemeContract
 import kotlin.math.roundToInt
